7370c4e009e0123c599180d9d336cd9f5ffeb4d7
nibble0101 <mj.mawa@yahoo.com>
2020-07-01 18:53:28 +0300

    Worked on visualization

--- src/main/visualization/Visualization.js
+++ src/main/visualization/Visualization.js
@@ -1,12 +1,169 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useRef } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson";
 import Zoom from "react-reveal/Zoom";
+import "./visualization.css";
 import { context } from "../../store";
 
+const graphParams = {
+  width: 900,
+  height: 400,
+  padTop: 50,
+  padBottom: 50,
+  padLeft: 50,
+  padRight: 50,
+};
+const { width, height, padTop, padBottom, padLeft, padRight } = graphParams;
+const legendParams = {
+  width: 200,
+  height: 20,
+};
+
 function Visualization(props) {
-  const { data } = useContext(context);
+  const { data, mapData } = useContext(context);
+  useEffect(() => {
+    const formatDate = d3.timeFormat("%B %d, %Y");
+    const formatDateTime = d3.timeFormat("%B %d, %Y  %H:%M:%S %p");
+    const dateToday = new Date();
+    const wrapper = d3.select("#visualization-wrapper");
+    wrapper
+      .append("h1")
+      .attr("class", "main-header")
+      .text("Covid-19 World Statistics");
+    const toolTip = wrapper.append("div").attr("class", "tooltip");
+    const svg = wrapper
+      .append("svg")
+      .attr("width", width + padLeft + padRight)
+      .attr("height", height + padTop + padBottom);
+
+    
+
+      function mouseoverHandler() {
+        d3.select(this).style("fill", "#303145");
+        const name = d3.select(this).attr("name");
+        const toolTip = d3.select(".tooltip");
+        var data = d3.select(this).attr("data");
+        data = JSON.parse(data);
+        if (data === null) {
+          toolTip.append("h2").text(name);
+          toolTip.append("div").text("Date: " + formatDate(dateToday));
+          toolTip.append("div").text("No data available");
+      
+          toolTip.style("opacity", "1");
+          toolTip.style("top", d3.event.pageY + "px");
+          toolTip.style("left", d3.event.pageX - 200 + "px");
+        } else {
+          toolTip.append("h2").text(name);
+          toolTip.append("div").attr("class", "wrapper-1");
+          d3.select(".wrapper-1")
+            .append("span")
+            .text("Date: ")
+            .style("color", "#6ed6ef");
+          d3.select(".wrapper-1")
+            .append("span")
+            .text(formatDate(dateToday));
+          toolTip.append("div").attr("class", "wrapper-2");
+          d3.select(".wrapper-2")
+            .append("span")
+            .text("Cases: ")
+            .style("color", "#6ed6ef");
+          d3.select(".wrapper-2")
+            .append("span")
+            .text((d3.format(",")(data.cases)) + "  +" + (d3.format(",")(data.todayCases)));
+          toolTip.append("div").attr("class", "wrapper-3");
+          d3.select(".wrapper-3")
+            .append("span")
+            .text("Deaths: ")
+            .style("color", "#6ed6ef");
+          d3.select(".wrapper-3")
+            .append("span")
+            .text((d3.format(",")(data.deaths)) + "  +" + (d3.format(",")(data.todayDeaths)) );
+          toolTip.append("div").attr("class", "wrapper-4");
+          d3.select(".wrapper-4")
+            .append("span")
+            .text("Recoveries: ")
+            .style("color", "#6ed6ef");
+          d3.select(".wrapper-4")
+            .append("span")
+            .text((d3.format(",")(data.recovered)) + "  +" + (d3.format(",")(data.todayRecovered)));
+          toolTip.style("opacity", "1");
+          toolTip.style("top", d3.event.pageY + "px");
+          toolTip.style("left", d3.event.pageX - 200 + "px");
+        }
+      }
+
+
+     function mouseoutHandler() {
+        const data = JSON.parse(d3.select(this).attr("data"));
+        var color;
+        if (!data) {
+          color = "#947b3cb5";
+        } else {
+          color = colorScale(+data.cases);
+        }
+        d3.select(this).style("fill", color);
+        const toolTip = d3.select(".tooltip");
+        toolTip.style("opacity", "0");
+        toolTip.select("h2").remove();
+        toolTip.selectAll("div").remove();
+      }
+
+
+
+
+
+    const colorScale = d3.scaleSequential();
+    const projection = d3.geoNaturalEarth1().scale(150);
+    const pathGenerator = d3.geoPath().projection(projection);
+    svg
+      .append("path")
+      .attr("class", "sphere")
+      .attr("d", pathGenerator({ type: "Sphere" }));
+    const countries = topojson.feature(mapData, mapData.objects.countries);
+    let highestCases, lowestCases;
+    data.forEach((country, index) => {
+        if(!index){
+            highestCases = country.cases;
+            lowestCases = country.cases;
+        }else{
+            highestCases = country.cases > highestCases? country.cases: highestCases;
+            lowestCases = country.cases < lowestCases? country.cases: lowestCases;
+        }
+    });
+    colorScale.domain([lowestCases, highestCases]).interpolator(d3.interpolatePiYG);
+    const paths = svg
+      .selectAll("path")
+      .data(countries.features)
+      .enter()
+      .append("path")
+      .attr("class", "country")
+      .attr("d", (d) => pathGenerator(d))
+      .attr("map-id", (d) => d.id)
+      .attr("name", (d) => d.properties.name)
+      .attr("data", (d, i) => {
+        const mydata = data.find((c) => {
+          return +d.id === +c.countryInfo._id;
+        });
+        if (mydata) {
+          return JSON.stringify(mydata);
+        }
+        return undefined;
+      })
+      .style("transition", "fill 500ms linear")
+      .attr("fill", function (d, i) {
+        const data = d3.select(this).attr("data");
+        if (!data) {
+          return "#947b3cb5";
+        }
+        const cases = JSON.parse(data).cases;
+        return colorScale(+cases);
+      })
+      .on("mouseover", mouseoverHandler)
+      .on("mouseout", mouseoutHandler);
+  }, []);
   return (
     <Zoom>
-      <h1> Visualization </h1>
+      <div id="visualization-wrapper"></div>
     </Zoom>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState, useReducer } from "react";
+import {json} from "d3";
 import { paginationReducer } from "./page-reducer";
 
-const url = "https://disease.sh/v2/countries";
+const urlCountries = "https://disease.sh/v2/countries";
+const urlAggregate = "https://disease.sh/v2/all";
+const urlMap = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 const context = React.createContext();
 function ContextProvider(props) {
   const [data, setData] = useState(null);
   const [pages, pageDispatch] = useReducer(paginationReducer, null);
   const [activeMenu, setActiveMenu] = useState("dashboard");
+  const [mapData, setMapData] = useState(null);
+  const [aggregateData, setAggregateData] = useState(null);
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        pageDispatch({ type: "set-pages", payload: data.length });
-      })
-      .catch((error) => error.message);
+    Promise.all([json(urlCountries), json(urlAggregate), json(urlMap)])
+    .then(data => {
+        pageDispatch({ type: "set-pages", payload: data[0].length });
+        setData(data[0]);
+        setAggregateData(data[1]);
+        setMapData(data[2]);
+    }).catch(error => console.log(error.message));
   }, []);
   function currentPageHandler(e) {
     pageDispatch({ type: "set-current-page", payload: +e.target.value });
@@ -60,6 +65,8 @@ function ContextProvider(props) {
         data,
         pages,
         activeMenu,
+        mapData,
+        aggregateData,
         currentPageHandler,
         nextChapterHandler,
         previousChapterHandler,
