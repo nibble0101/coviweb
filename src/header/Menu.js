05fc1742e28b0aed41670403206de2eca9c26360
nibble0101 <mj.mawa@yahoo.com>
2020-07-04 23:34:31 +0300

    Worked on hamburger menu

--- src/header/Menu.js
+++ src/header/Menu.js
@@ -1,15 +1,31 @@
 import React, {useContext} from "react";
 import {context} from "../store";
+import HamburgerMenu from "react-hamburger-menu";
 function Menu(props) {
   const { menuClickHandler} = useContext(context);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const myRef = React.useRef();
+  const hambugerClickHandler = React.useCallback(() => {
+    setIsOpen(!isOpen);
+    document.getElementById("menu").classList.toggle("display-menu")
+  });
   return (
-    <div className="menu" onClick = {menuClickHandler}>
-      <ul>
+    <div className="menu" >
+      <ul id = "menu" ref = {myRef} onClick = {menuClickHandler}>
         <li id="dashboard"> Dashboard </li>
         <li id="about"> About </li>
         <li id="visualization"> Visualization </li>
         <li id="press"> Press </li>
       </ul>
+      <HamburgerMenu
+        isOpen={isOpen}
+        menuClicked={hambugerClickHandler}
+        width={18}
+        height={10}
+        strokeWidth={2}
+        color={"#46cf73"}
+        className={"hamburger-menu-icon"}
+      />
     </div>
   );
 }
