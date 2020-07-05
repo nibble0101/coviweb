295b6ee5834fbe09588dd9e0bce8f48fffc83028
nibble0101 <mj.mawa@yahoo.com>
2020-07-05 22:56:41 +0300

    Modified how to reference api key

--- src/store.js
+++ src/store.js
@@ -16,7 +16,7 @@ function ContextProvider(props) {
   const [aggregateData, setAggregateData] = useState(null);
   const [articles, setArticles] = useState(null);
   useEffect(() => {
-    Promise.all([json(urlCountries), json(urlAggregate), json(urlMap), json(urlPress + process.env.API_KEY)])
+    Promise.all([json(urlCountries), json(urlAggregate), json(urlMap), json(urlPress + API_KEY)])
     .then(data => {
         pageDispatch({ type: "set-pages", payload: data[0].length });
         setData(data[0]);
