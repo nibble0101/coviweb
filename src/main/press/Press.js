99311054773a8d6080a02c190060da65a4b73b4d
nibble0101 <mj.mawa@yahoo.com>
2020-08-29 19:51:47 +0300

    Worked on data fetching bug in Press component

--- src/main/press/Press.js
+++ src/main/press/Press.js
@@ -4,28 +4,48 @@ import NewYorkTimes from "./NewYorkTimes";
 import LoadMoreArticles from "./LoadMoreArticles";
 import Loader from "../../loader/Loader";
 import urlFactory from "./url-factory";
+import { max } from "d3";
 
+function readPage() {
+  const articles = JSON.parse(localStorage.getItem("articles"));
+  if (articles && articles.length) {
+    const page = articles.length / 10;
+    if (Number.isInteger(page)) {
+      return page - 1;
+    }
+    return Math.floor(page);
+  }
+  return 0;
+}
 function Press(props) {
   const [articles, setArticles] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(readPage);
   const [maxPage, setMaxPage] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  const pageHandle = useCallback((event) => {
-    if (page === maxPage) {
-      return;
+  const pageHandle = useCallback(() => {
+    if(maxPage === articles.length){
+        return;
     }
     setPage((prevPage) => prevPage + 1);
   });
 
   useEffect(() => {
-    if (!page) {
-      const cachedData = JSON.parse(localStorage.getItem("articles"));
-      if (cachedData && cachedData.length) {
-        setArticles((prevArticles) => [
-          ...prevArticles,
-          ...cachedData
-        ]);
-        return;
+    const cachedData = JSON.parse(localStorage.getItem("articles"));
+    if (cachedData && cachedData.length) {
+      setIsLoading(true)
+      const offsetPage = cachedData.length / 10;
+      if (Number.isInteger(offsetPage)) {
+        if (page === offsetPage - 1) {
+          setArticles([...cachedData]);
+          setIsLoading(false);
+          return;
+        }
+      } else {
+        if (page === Math.floor(offsetPage)) {
+          setArticles([...cachedData]);
+          setIsLoading(false);
+          return;
+        }
       }
     }
     urlFactory.currentPage = page;
@@ -40,10 +60,10 @@ function Press(props) {
               "articles",
               JSON.stringify([...prevArticles, ...data.response.docs])
             );
+            localStorage.setItem("current-page", JSON.stringify(page));
             return [...prevArticles, ...data.response.docs];
           });
-          const { hits } = data.response.meta;
-          setMaxPage(Math.floor(hits / 10));
+          setMaxPage(data.response.meta.hits);
         });
       setIsLoading(false);
     }
