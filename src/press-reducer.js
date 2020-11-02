e35de3b647155ec96afc6aa0648a29a4fb25c4eb
nibble0101 <mj.mawa@gmail.com>
2020-11-02 11:24:17 +0300

    Added isLoading field to initial press state

--- src/press-reducer.js
+++ src/press-reducer.js
@@ -1,14 +1,26 @@
-const initialPressState = {page: 0, articles: []};
-
-function pressReducer(state, action) {
-    switch(action.type){
-       case "SET_ARTICLES":
-           return {...state, articles: [...state.articles, ...action.articles]};
-       case "SET_PAGE":
-           return {...state, page: state.page + 1};
-       default:
-           return {...state};
-    }
+const initialPressState = {
+  page: 0,
+  pageCount: null,
+  articles: [],
+  isLoading: false,
 };
 
-export {initialPressState, pressReducer};
+function pressReducer(state, action) {
+  switch (action.type) {
+    case "SET_ARTICLES":
+      return {
+        ...state,
+        articles: [...state.articles, ...action.articles],
+      };
+    case "SET_PAGE":
+      return { ...state, page: state.page + 1 };
+    case "SET_TOTAL_PAGE_COUNT":
+      return { ...state, pageCount: action.pageCount };
+    case "SET_LOADING_FLAG":
+      return { ...state, isLoading: action.isLoading };
+    default:
+      return { ...state };
+  }
+}
+
+export { initialPressState, pressReducer };
